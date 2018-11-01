0e3f3fe0e5371aad45f4998557f0563717108211
piaxar <i.a.panchenko@icloud.com>
2018-11-01 19:15:15 +0300

    Move cell operates on GameState

--- src/Tetris/GameLogic.hs
+++ src/Tetris/GameLogic.hs
@@ -13,19 +13,65 @@ handleGameState :: Event -> GameState -> GameState
 handleGameState _ = id
 
 updateGameState :: GameState -> GameState
-updateGameState (GameState field tetromino cells)
-  | isFinished curState = curState
-  | otherwise = GameState field (moveTetromino fallSpeed tetromino) cells
+updateGameState gs
+  | isFinished gs = gs
+  | otherwise = (removeFilledRows
+               . generateTetromino
+               . handleCollision
+               . moveTetromino ) gs
+
+
+-- / Moves tetromino according to velocity
+moveTetromino :: GameState -> GameState
+moveTetromino (GameState field Nothing cells)
+  = (GameState field Nothing cells)
+moveTetromino (GameState field (Just tetromino) cells)
+  = (GameState field movedTetromino cells)
   where
-    curState = (GameState field tetromino cells)
+    (Tetromino (x, y) rCells) = tetromino
+    (dx, dy) = fallSpeed
 
-moveTetromino :: Velocity -> Maybe Tetromino -> Maybe Tetromino
-moveTetromino _ Nothing = Nothing
-moveTetromino (dx, dy) (Just (Tetromino (x, y) cells))
-  = Just (Tetromino (x + dx, y + dy) cells)
+    movedTetromino = Just (Tetromino (x + dx, y + dy) rCells)
 
+
+-- / Check position of tetromino, if touches objects or bottom line add to cells
 handleCollision :: GameState -> GameState
-handleCollision = id
+handleCollision (GameState field Nothing cells)
+  = GameState field Nothing cells
+handleCollision (GameState field (Just tetromino) cells)
+  | touchesBottom = GameState field Nothing cells
+--  | touchesCells =
+  | otherwise = GameState field (Just tetromino) cells
+  where
+    (Tetromino (posX, posY) rCells) = tetromino
+    absCells = toCells (posX, posY) rCells
+
+    touchesBottom
+      | posY == 0 = True
+      | otherwise = False
+
+    touchesCells = False
+    
+    toCells :: Position -> [RelativeCell] -> [Cell]
+    toCells (x, y)
+      = map (\(RelativeCell (dx, dy) c) -> Cell (x + dx, y + dy) c)
+
+
+-- onlyRows :: [Cell] -> [Integer]
+-- onlyRows = map (\(Cell (_, y) _) -> y)
+--
+-- cellsInCol :: Integer -> [Cell] -> [Cell]
+-- cellsInCol col = filter (\(Cell (x, _ ) _) -> x == col)
+--
+--
+
+--
+-- ff = [(Cell (10, 1) yellow),
+--       (Cell (10, 2) yellow),
+--       (Cell (10, 5) yellow),
+--       (Cell (4, 1) yellow),
+--       (Cell (5, 2) yellow)]
+
 
 removeFilledRows :: GameState -> GameState
 removeFilledRows = id
