5d378e32e9960a98a5940a7e96e994d0886098ba
piaxar <i.a.panchenko@icloud.com>
2018-11-22 21:22:11 +0300

    Pretty view

--- src/Tetris/Render.hs
+++ src/Tetris/Render.hs
@@ -20,7 +20,8 @@ renderGameState (GameState field tetromino cells _ score)
         =  renderTetromino tetromino
         <> renderCells cells
         <> renderField backgroundColor field
-        <> translated (w + 2) (h - 2) (renderScore score)
+        <> translated (w + 3) (h - 2) (renderScore score)
+        <> translated (-1) (-1) (renderDecorations 18 24)
 
       Field height width = field
       w = fromInteger width
@@ -48,9 +49,18 @@ renderCell (Cell (x, y) c) = colored c (translated i j (solidRectangle 1.0 1.0))
    where
     (i, j) = intsToDoubles (x, y)
 
+-- | Render scoring
 renderScore :: Integer -> Picture
-renderScore = lettering . fromString . show
+renderScore n = (lettering . fromString) ("Score: " ++ show n)
 
+-- | Render decorations in upper-right direction
+-- | w = 18, h = 24
+renderDecorations :: Double -> Double -> Picture
+renderDecorations w h
+  = translated (w/2 - 0.5) (h/2 - 0.5) (mBack <> mBorder)
+  where
+    mBack   = colored (grey 0.85) (solidRectangle w h)
+    mBorder = colored black (thickRectangle 0.2 w h)
 
 relativeToAbs :: Position -> RelativeCell -> Cell
 relativeToAbs (x, y) (RelativeCell (dx, dy) color)
